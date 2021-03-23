42adddbf8c0a2450ebeaa92ac833404053a0c89f
Ryan Richoux <Ryan.Richoux@Gmail.com>
2021-03-23 00:13:43 -0500

    First big push.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Link, Switch, Route} from "react-router-dom"
+import {getToken, clearToken} from "./auth"
+import{fetchUserData, fetchAllActivites} from "./api"
+import {
+  Login,
+  Register,
+  Home
+} from "./components"
 
-function App() {
+const App = () => {
+  const [authorized, setAuthorized] = useState(false);
+  const [currentUser, setCurrentUser] = useState("");
+  const [loggedIn, setLoggedIn] = useState(getToken());
+
+  
+  // useEffect(async () => {
+  //   if (loggedIn) {
+  //       try {
+  //           const data = await fetchUserData();
+  //           setCurrentUser(data.username);
+  //           const grabbedActivities = await fetchAllActivites();
+  //           setActivities(grabbedActivities);
+
+  //       } catch (error) {
+  //           console.error(error);
+  //       }
+  //   }
+  // }, [loggedIn])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navBar">
+        <h1>Spend Money!</h1>
+        <div>
+          <Link className="Link" to= '/'>Home</Link>
+          {/* !authorized  */ !loggedIn ? (<Link className="Link" to= '/Login'>Login</Link>) : null}
+          {/* !authorized  */ !loggedIn ? (<Link className="Link" to= '/Register'>Sign Up</Link>) : null}
+          {loggedIn ? <Link className="Link" onClick={() => {
+                        clearToken();
+                        
+                        setLoggedIn(null);
+                        setAuthorized(null)
+                        setCurrentUser(null)
+                    }}
+                        to='/'>Log Out</Link> : null}
+        </div>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path= '/'>
+            <Home />
+          </Route>
+          <Route path='/Login'>
+              <Login
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+                setCurrentUser={setCurrentUser}
+                setAuthorized={setAuthorized}
+                authorized={authorized}
+              />
+          </Route>
+          <Route path='/Register'>
+             <Register
+             setAuthorized={setAuthorized} 
+             loggedIn={loggedIn}
+             setLoggedIn={setLoggedIn}
+             /> 
+          </Route>
+          
+         
+        </Switch>  
+      </main>
+    </Router>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { storeToken } from "../auth";
+import {Redirect} from "react-router-dom"
+
+const Login = (props) => {
+  const [user, setUser] = useState("");
+  const { setAuthorized, setCurrentUser, loggedIn, setLoggedIn } = props;
+
+  function helperHandleSubmit(e) {
+    setUser({ ...user, password: e.target.value });
+    setCurrentUser(user.username);
+    /* console.log(currentUser); */
+  }
+
+  
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    fetch(
+      "https://fitnesstrac-kr.herokuapp.com/api/users/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username: user.username, password: user.password }),
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+
+        if (result.message === "you're logged in!") {
+          alert(result.message);
+          setAuthorized(result.token)
+          setLoggedIn(result.token)
+          storeToken(result.token);
+        } else {
+          alert(result.message);
+          
+        }
+      })
+      .catch(console.error);
+  };
+  if (loggedIn) {
+    return <Redirect to="/" />
+  }else{
+    return (
+      <form onSubmit={handleSubmit}>
+        <h1> Login:</h1>
+        <label>Username:</label>
+        <input
+          name="Username"
+          required
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+        />
+        <label>Password:</label>
+        <input type="password" required onChange={(e) => helperHandleSubmit(e)} />
+        <button type="submit">submit</button>
+      </form>
+    );
+  }
+};
+
+export default Login;
